9ab98dfc205ba99b9246d325816ebe17c239c210
Obelisk <352627129@qq.com>
2016-09-14 17:15:46 +0800

    测试wap线上情况

--- web/index.php
+++ web/index.php
@@ -4,12 +4,12 @@ header("Content-Type:text/html;charset=utf-8");
 defined('YII_DEBUG') or define('YII_DEBUG', 1);
 defined('YII_ENV') or define('YII_ENV', '1');
 //
-var_dump(__DIR__);die;
+
 require(__DIR__ . '/../vendor/autoload.php');
 
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
-
+var_dump($config);die;
 //增加移动端访问代码
 require(__DIR__ . '/../libs/Mobile_Detect.php');
 
