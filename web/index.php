08e312e71e7e5e5af3cb6e4c47358ac82acf289b
Obelisk <352627129@qq.com>
2016-09-14 17:17:49 +0800

    测试wap线上情况

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ require(__DIR__ . '/../vendor/autoload.php');
 
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
-var_dump($config);die;
+
 //增加移动端访问代码
 require(__DIR__ . '/../libs/Mobile_Detect.php');
 
