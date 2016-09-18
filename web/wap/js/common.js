1f1fb794638db1aa923a19590878a774083ddcef
Anne <anne.z@thinkwithu.com>
2016-09-18 15:08:38 +0800

    修改图片自适应

--- web/wap/js/common.js
+++ web/wap/js/common.js
@@ -8,10 +8,21 @@
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
             docEl.style.fontSize = 40 * (clientWidth / 750) + 'px';
-            console.log(40 * (clientWidth / 750))
+            //console.log(40 * (clientWidth / 750))
         };
 
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+//设置自适应圆形高度
+window.onload=function(){
+    var ct_he=$(".ct-head");
+    var tc_im=$(".tc-img");
+    if(ct_he){
+        ct_he.css("height",ct_he.css("width"));
+    }
+    if(tc_im){
+        tc_im.css("height",tc_im.css("width"));
+    }
+};
