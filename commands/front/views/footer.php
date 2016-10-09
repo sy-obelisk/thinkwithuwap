182846bd7827cd5805c6ba02d6d1c2e3e077f095
Obelisk <352627129@qq.com>
2016-10-09 15:35:50 +0800

    代码修改

--- commands/front/views/footer.php
+++ commands/front/views/footer.php
@@ -9,7 +9,7 @@
         <p class="ft-name">电话咨询</p>
     </a>
     <img src="/wap/images/line.png" style="height: 1.35rem" alt="">
-    <a class="inb f-list" href="http://looyuoms7731.looyu.com/chat/chat/p.do?_server=0&c=20001277&f=10057560&g=10060013&refer=meiguoliuxue">
+    <a class="inb f-list" target="_self" onclick="doyoo.util.openChat('g=10060013');return false;">
         <img src="/wap/images/icon-18.png" style="width: 1.15rem" alt="">
         <p class="ft-name">在线咨询</p>
     </a>
