d59e9ca75d749989d3019fea5b994f7c8df8c1ee
sy <sy@gmail.com>
2016-10-25 11:21:59 +0800

    test

--- modules/wap/views/cultivate/index.php
+++ modules/wap/views/cultivate/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!--阻止浏览器缓存-->
+    <!--阻止浏览器缓�?->
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
     <meta http-equiv="expires" content="0">
@@ -15,14 +15,14 @@
     <meta name="author" content="">
     <meta name="Copyright" content="">
     <!-- <meta name="description" content=""> -->
-    <!-- 让IE浏览器用最高级内核渲染页面 还有用 Chrome 框架的页面用webkit 内核
+    <!-- 让IE浏览器用最高级内核渲染页面 还有�?Chrome 框架的页面用webkit 内核
     ================================================== -->
     <meta http-equiv="X-UA-Compatible" content="chrome=1,IE=edge">
-    <!-- IOS6全屏 Chrome高版本全屏
+    <!-- IOS6全屏 Chrome高版本全�?
     ================================================== -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
-    <!-- 让360双核浏览器用webkit内核渲染页面
+    <!-- �?60双核浏览器用webkit内核渲染页面
     ================================================== -->
     <meta name="renderer" content="webkit">
     <!-- Mobile Specific Metas
@@ -45,91 +45,82 @@
 <!--内页头部-->
 <header class=" pd-0 bg-1">
     <div class="header-2">
-<!--        <a class="tb tl" href="javascript:history.go(-1)"><img class="reBack" src="/wap/images/reBack.png" alt=""></a>-->
+        <a class="tb tl" href="javascript:history.go(-1)"><img class="reBack" src="/wap/images/reBack.png" alt=""></a>
         <span class=" tb header-tit tm">GMAT课程</span>
-<!--        <a class="tr tb" href="/menu.html">-->
-<!--            <img class="menu-icon" src="/wap/images/menu.png" alt="">-->
-<!--        </a>-->
+        <a class="tr tb" href="/menu.html">
+            <img class="menu-icon" src="/wap/images/menu.png" alt="">
+        </a>
     </div>
 </header>
 <!--内页头部 END-->
-<!--广告位-->
+<!--广告�?->
 <div class="mg-t1"><a href="#"><img src="/wap/images/ad-9.png" alt=""></a></div>
-<!--广告位 END-->
+<!--广告�?END-->
 <!--栏目-->
 <div class="pd1 mg-t1 bg-col-1">
-    <p class="col-name inb">选择申友的理由</p>
+    <p class="col-name inb">选择申友的理�?/p>
 </div>
 <div class="bg-1 pd-1" style="padding-bottom: 1rem">
     <img src="/wap/images/sub-7.png" alt="">
 </div>
 <div class="pd1 mg-t1 bg-col-1">
-    <p class="col-name inb">申友GMAT特色课程,45天快速突破700+</p>
+    <p class="col-name inb">申友GMAT特色课程,45天快速突�?00+</p>
 </div>
 <div class="sub-list bg-1">
     <div class="clearfix">
-<<<<<<< HEAD
-        <a class="sub-1 fl inb" href="http://p.qiao.baidu.com/im/index?siteid=9946474&ucid=3827656&cp=&cr=&cw="><img src="/wap/images/sub-1.png" alt=""></a>
-        <a class="sub-2 fr inb" href="http://p.qiao.baidu.com/im/index?siteid=9946474&ucid=3827656&cp=&cr=&cw="><img src="/wap/images/sub-2.png" alt=""></a>
+        <a class="sub-1 fl inb" href="/gmat-week.html"><img src="/wap/images/sub-1.png" alt=""></a>
+        <a class="sub-2 fr inb" href="/gmat-vip.html"><img src="/wap/images/sub-2.png" alt=""></a>
     </div>
     <div class="clearfix">
-        <a class="sub-3 fl inb" href="http://p.qiao.baidu.com/im/index?siteid=9946474&ucid=3827656&cp=&cr=&cw="><img src="/wap/images/sub-3.png" alt=""></a>
-        <a class="sub-4 fr inb" href="http://p.qiao.baidu.com/im/index?siteid=9946474&ucid=3827656&cp=&cr=&cw="><img src="/wap/images/sub-4.png" alt=""></a>
-=======
-        <a class="sub-1 fl inb" target="_self" href="http://looyuoms7714.looyu.com/chat/chat/p.do?g=10060013&md=2&c=20001277&v=6bceb2ee258de2e8d5c5f1d0fde86bd3aa&u=74942c0766194e3715e689a837ebea1202&f=10066142&site=0&p0=http%3A%2F%2Fthinkwap.cc%2F&ct=3&refer=&loc=http%3A%2F%2Fthinkwap.cc%2F&_d=1476001546707"><img src="/wap/images/sub-1.png" alt=""></a>
-        <a class="sub-2 fr inb" target="_self" href="http://looyuoms7714.looyu.com/chat/chat/p.do?g=10060013&md=2&c=20001277&v=6bceb2ee258de2e8d5c5f1d0fde86bd3aa&u=74942c0766194e3715e689a837ebea1202&f=10066142&site=0&p0=http%3A%2F%2Fthinkwap.cc%2F&ct=3&refer=&loc=http%3A%2F%2Fthinkwap.cc%2F&_d=1476001546707"><img src="/wap/images/sub-2.png" alt=""></a>
-    </div>
-    <div class="clearfix">
-        <a class="sub-3 fl inb" target="_self" href="http://looyuoms7714.looyu.com/chat/chat/p.do?g=10060013&md=2&c=20001277&v=6bceb2ee258de2e8d5c5f1d0fde86bd3aa&u=74942c0766194e3715e689a837ebea1202&f=10066142&site=0&p0=http%3A%2F%2Fthinkwap.cc%2F&ct=3&refer=&loc=http%3A%2F%2Fthinkwap.cc%2F&_d=1476001546707"><img src="/wap/images/sub-3.png" alt=""></a>
-        <a class="sub-4 fr inb" target="_self" href="http://looyuoms7714.looyu.com/chat/chat/p.do?g=10060013&md=2&c=20001277&v=6bceb2ee258de2e8d5c5f1d0fde86bd3aa&u=74942c0766194e3715e689a837ebea1202&f=10066142&site=0&p0=http%3A%2F%2Fthinkwap.cc%2F&ct=3&refer=&loc=http%3A%2F%2Fthinkwap.cc%2F&_d=1476001546707"><img src="/wap/images/sub-4.png" alt=""></a>
->>>>>>> 691b1be3e04db3beceaba1842ed135b0f33284b0
+        <a class="sub-3 fl inb" href="/gmat-summer.html"><img src="/wap/images/sub-3.png" alt=""></a>
+        <a class="sub-4 fr inb" href="#"><img src="/wap/images/sub-4.png" alt=""></a>
     </div>
 </div>
 <!--栏目 END-->
 <div class="pd1 mg-t1 bg-col-1" name="gmat-open" id="gmat-open">
-    <p class="col-name inb">最新开班查询</p>
+    <p class="col-name inb">最新开班查�?/p>
 </div>
 <ul class="bg-1 pd-0 open-list">
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
             <h1 class="open-name">GMAT强化班（周末班） </h1>
-            <P class="open-time">10月15、16、22、23、29、30日</P>
+            <P class="open-time">每月周末滚动开�?/P>
         </div>
     </li>
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
-            <h1 class="open-name">GMAT国庆班 </h1>
-            <P class="open-time">10月1、2、3、4、5、6日（全日制）</P>
+            <h1 class="open-name">GMAT国庆�?</h1>
+            <P class="open-time">10�?�?�?�?�?�?日（全日制）</P>
         </div>
     </li>
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
-            <h1 class="open-name">GMAT寒假班  </h1>
-            <P class="open-time">1、2月每周滚动开班；连授班</P>
+            <h1 class="open-name">GMAT寒假�? </h1>
+            <P class="open-time">1�?月每周滚动开班；连授�?/P>
         </div>
     </li>
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
             <h1 class="open-name">GMAT VIP一对一 </h1>
-            <P class="open-time">随时开课</P>
+            <P class="open-time">随时开�?/P>
         </div>
     </li>
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
-            <h1 class="open-name">GMAT PRE基础班 </h1>
-            <P class="open-time">随时开课</P>
+            <h1 class="open-name">GMAT PRE基础�?</h1>
+            <P class="open-time">随时开�?/P>
         </div>
     </li>
     <li>
         <div class="fl open-img"><img src="/wap/images/sub-5.png" alt=""></div>
         <div class="fr open-text">
-            <h1 class="open-name">GMAT机经冲刺班 </h1>
-            <P class="open-time">每月换库后开课</P>
+            <h1 class="open-name">GMAT机经冲刺�?</h1>
+            <P class="open-time">每月换库后开�?/P>
         </div>
     </li>
 
@@ -138,33 +129,33 @@
     <p class="col-name inb">选择申友GMAT课程，轻松拿高分</p>
     <ul class="course-wrap tm">
         <li>
-            <p class="course-tit">测评预<br>约访谈</p>
-            <p class="course-de">名师剖析测评结果，针对性提出复习建议</p>
-            <p class="course-time">（2课时）</p>
+            <p class="course-tit">测评�?br>约访�?/p>
+            <p class="course-de">名师剖析测评结果，针对性提出复习建�?/p>
+            <p class="course-time">�?课时�?/p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
         <li>
             <p class="course-tit">课前<br>预习</p>
             <p class="course-de">GMAT强化课前预习，掌握考试基础知识</p>
-            <p class="course-time">（30课时）</p>
+            <p class="course-time">�?0课时�?/p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
         <li>
             <p class="course-tit">核心<br>课程</p>
-            <p class="course-de">指导 GMAT 核心内容的解题思路和方法</p>
-            <p class="course-time">（30课时）</p>
+            <p class="course-de">指导 GMAT 核心内容的解题思路和方�?/p>
+            <p class="course-time">�?0课时�?/p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
         <li>
             <p class="course-tit">真题<br>精讲</p>
-            <p class="course-de">远程线上讲解，做题中巩固思路和方法</p>
-            <p class="course-time">（32课时）</p>
+            <p class="course-de">远程线上讲解，做题中巩固思路和方�?/p>
+            <p class="course-time">�?2课时�?/p>
         </li>
-        <!--        <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
+<!--        <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
         <li>
-            <p class="course-tit">模考<br>解析</p>
-            <p class="course-de">学管老师发送模考卷， 远程线上分析讲解</p>
-            <p class="course-time">（6课时）</p>
+            <p class="course-tit">模�?br>解析</p>
+            <p class="course-de">学管老师发送模考卷�?远程线上分析讲解</p>
+            <p class="course-time">�?课时�?/p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
         <li>
@@ -173,18 +164,18 @@
             <p class="course-time">（免费赠送）</p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
-        <!--        <div class="inb sub-8" style=" margin: 0.5rem 0.3rem 0;"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
+<!--        <div class="inb sub-8" style=" margin: 0.5rem 0.3rem 0;"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
         <li>
             <p class="course-tit">学习<br>社区</p>
             <p class="course-de">成为申友学员并开通账号后即可登陆使用</p>
             <p class="course-time">（免费赠送）</p>
         </li>
         <div class="inb sub-8"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>
-        <!--        <div class="inb sub-8" style=" margin: 0.5rem 0.3rem 0;"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
+<!--        <div class="inb sub-8" style=" margin: 0.5rem 0.3rem 0;"><img src="/wap/images/sub-8.png" style="width: 1.1rem" alt=""></div>-->
         <li>
             <p class="course-tit">机经<br>冲刺</p>
             <p class="course-de">当月题库讲解机经，助大家进行考前冲刺</p>
-            <p class="course-time">（换库后）</p>
+            <p class="course-time">（换库后�?/p>
         </li>
     </ul>
 </div>
@@ -201,17 +192,13 @@
 
                 <p class="ct-school ellipsis"><?php echo $v['abroadSchool']?></p>
             </li>
-        <?php
-        }
+            <?php
+            }
         ?>
 
     </ul>
     <div class="tm ct-btn mg-t1">
-<<<<<<< HEAD
-        <a class="ct-b1 inb" href="http://p.qiao.baidu.com/im/index?siteid=9946474&ucid=3827656&cp=&cr=&cw=">点击了解：查看更多申友名校案例</a>
-=======
-        <a class="ct-b1 inb" target="_self" href="http://looyuoms7714.looyu.com/chat/chat/p.do?g=10060013&md=2&c=20001277&v=6bceb2ee258de2e8d5c5f1d0fde86bd3aa&u=74942c0766194e3715e689a837ebea1202&f=10066142&site=0&p0=http%3A%2F%2Fthinkwap.cc%2F&ct=3&refer=&loc=http%3A%2F%2Fthinkwap.cc%2F&_d=1476001664688">点击了解：查看更多申友名校案例</a>
->>>>>>> 691b1be3e04db3beceaba1842ed135b0f33284b0
+        <a class="ct-b1 inb" href="/case.html">点击了解：查看更多申友名校案�?/a>
     </div>
 </div>
 <div class="bg-1 mg-t1">
@@ -257,7 +244,7 @@
 </div>
 <!--栏目-->
 <div class="pd1 mg-t1 bg-col-1" style="padding-bottom: 0">
-    <p class="col-name inb">每日一题</p>
+    <p class="col-name inb">每日一�?/p>
     <div class="inb fr">
         <div class="icon inb">
             <span class=" r-name inb">全部 ></span>
@@ -275,11 +262,11 @@
 
             <p class="ellipsis topic-de"><?php echo $v['abstract']?>......</p>
 
-            <p class="topic-time">时间：<?php echo $v['createTime']?></p>
+            <p class="topic-time">时间�??php echo $v['createTime']?></p>
         </li>
     <?php
     }
-    ?>
+?>
 
 </ul>
 
