f7e4aa01df57f8bcc3c52a98395241fded500c49
Obelisk <352627129@qq.com>
2017-03-23 10:52:27 +0800

    test

--- commands/front/views/footer.php
+++ commands/front/views/footer.php
@@ -19,3 +19,17 @@
         <p class="ft-name">全国分支</p>
     </a>
 </footer>
+<script>
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
+</script>
